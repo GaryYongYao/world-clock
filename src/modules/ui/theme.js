f8388593789da80bba15af8cf9f457741209463a
GaryYongYao <garyyongyao@gmail.com>
2020-09-26 01:41:27 +0800

    additional setup

---
+++ src/modules/ui/theme.js
@@ -0,0 +1,216 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import { primaryColor, textBlack, lightGrey, lightGreyO } from './colors'
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiAutocomplete: {
+      paper: {
+        backgroundColor: '#FFFFFF !important'
+      }
+    },
+    MuiFormControl: {
+      root: {
+        margin: '8px 0px',
+      },
+    },
+    MuiPickersBasePicker: {
+      container: {
+        backgroundColor: '#FFFFFF'
+      }
+    },
+    MuiFormLabel: {
+      root: {
+        color: '#707070',
+        fontSize: '1rem',
+        fontWeight: 500,
+        lineHeight: 1,
+        '&.Mui-focused': {
+          color: '#707070',
+        },
+      },
+    },
+    MuiInputBase: {
+      input: {
+        color: '#1a1a1a',
+        fontSize: '1rem',
+        fontWeight: 500,
+        lineHeight: 1
+      }
+    },
+    MuiFormControlLabel: {
+      root: {
+        color: '#707070'
+      }
+    },
+    MuiInputLabel: {
+      shrink: {
+        transform: 'translate(0, 1.5px) scale(0.9)'
+      }
+    },
+    MuiFormHelperText: {
+      root: {
+        lineHeight: '1.3em',
+        color: '#707070'
+      }
+    },
+    MuiInput: {
+      underline: {
+        '&:before': {
+          borderBottom: '1px solid #e4e4e4',
+        },
+        '&:after': {
+          borderBottom: `1px solid ${primaryColor}`,
+        },
+        '&:hover:not(.Mui-disabled):before': {
+          borderBottom: `1px solid ${primaryColor}`,
+        }
+      },
+    },
+    MuiButton: {
+      root: {
+        fontSize: '0.875rem',
+        fontWeight: 700,
+        lineHeight: 1.75,
+        textTransform: 'inherit',
+        minWidth: 85,
+        'text-transform': 'uppercase'
+      },
+      sizeSmall: {
+        'text-transform': 'none'
+      },
+      wrapper: {
+        fontSize: '0.875rem',
+        fontWeight: 700,
+        lineHeight: 1.75,
+      },
+      contained: {
+        boxShadow: 'none',
+        '&.Mui-disabled': {
+          backgroundColor: lightGreyO,
+          color: lightGrey
+        }
+      },
+      outlinedSizeSmall: {
+        padding: '2.7px 9px'
+      },
+    },
+    MuiCard: {
+      root: {
+        boxShadow: '0px 3px 6px #0000000A',
+      }
+    },
+    MuiTypography: {
+      useNextVariants: true,
+      body1: {
+        fontSize: '1rem',
+        fontWeight: 400,
+        lineHeight: 1.43,
+        letterSpacing: '0.01071em',
+        color: '#707070',
+      },
+      body2: {
+        fontSize: '0.875rem',
+        fontWeight: 400,
+        lineHeight: 1.43,
+        letterSpacing: '0.01071em',
+        color: '#707070',
+      },
+      h2: {
+        fontSize: '3.75rem',
+        fontWeight: 100,
+        lineHeight: 1,
+        color: '#707070',
+      },
+      h3: {
+        fontSize: '3rem',
+        fontWeight: 400,
+        lineHeight: 1.17,
+        color: '#707070',
+      },
+      h4: {
+        fontSize: '2.125rem',
+        fontWeight: 600,
+        lineHeight: 1.17,
+        color: '#707070',
+      },
+      h5: {
+        fontSize: '1.5rem',
+        fontWeight: 600,
+        lineHeight: 1.33,
+        letterSpacing: '0em',
+        color: '#707070',
+      },
+      h6: {
+        fontSize: '1.25rem',
+        fontWeight: 600,
+        lineHeight: 1.6,
+        letterSpacing: '0.0075em',
+        color: '#303030',
+      },
+      subtitle1: {
+        fontSize: '1rem',
+        fontWeight: 600,
+        lineHeight: 1.75,
+        letterSpacing: '0.00938em',
+        color: '#707070',
+      },
+      subtitle2: {
+        fontSize: '0.875rem',
+        fontWeight: 500,
+        lineHeight: 1.57,
+        letterSpacing: '0.00714em'
+      },
+      caption: {
+        fontSize: '0.75rem',
+        lineHeight: 1.66,
+        letterSpacing: '0.03333em'
+      },
+    },
+
+    MuiDrawer: {
+      paper: {
+        backgroundColor: '#FAFAFA'
+      }
+    },
+
+    MuiPopover: {
+      paper: {
+        boxShadow: '0px 5px 5px -3px rgba(0, 0, 0, 0.05), 0px 4px 7px 1px rgba(0, 0, 0, 0.01), 0px 3px 5px 1px rgba(0,0,0,0.12)',
+      }
+    },
+    MuiPaper: {
+      rounded: {
+        borderRadius: 8
+      },
+      elevation2: {
+        boxShadow: 'initial',
+        '& [class^="Component-horizontalScrollContainer-"]': {
+          overflowX: 'visible !important',
+          '& > div > div': {
+            overflowY: 'visible !important',
+          }
+        }
+      },
+      root: {
+        backgroundColor: 'transparent'
+      }
+    }
+  },
+  palette: {
+    primary: { main: primaryColor, text: '#707070' },
+    secondary: { main: textBlack },
+    error: { main: '#ff5a5f', text: '#707070' }
+  },
+  typography: {
+    useNextVariants: true,
+    fontFamily: 'Helvetica',
+    htmlFontSize: 14,
+  },
+  custom: {
+    authLayoutContainer: {
+      minHeight: '100vh',
+    },
+  },
+})
+
+export default theme
--- src/modules/utils/index.js
+++ src/modules/utils/index.js
@@ -2,14 +2,6 @@ import { lowerCase, startCase } from 'lodash'
 
 export const startCapital = text => startCase(lowerCase(text))
 
-export const formatDate = (date, seperator = '/') => {
-  const day = date.getDate()
-  const month = date.getMonth()
-  const year = date.getFullYear()
-
-  return `${twoDigit(day)}${seperator}${twoDigit(month + 1)}${seperator}${year}`
-}
-
 function twoDigit(num) {
   return (num < 10) ? `0${num}` : num
 }
